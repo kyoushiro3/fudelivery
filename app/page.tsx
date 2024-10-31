8ec587b426a4b32eb21bb926031d1e665beda9b1
kyoushiro3 <starkjarvis99@gmail.com>
2024-10-31 12:47:04 +0800

    added: style on the header and for the body

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,30 @@
-import AddFood from "./addFood/page";
+import GridPattern from "@/components/ui/grid-pattern";
+import { cn } from "@/libs/utils";
 
 export default function Home() {
   return (
-    <div className="relative px-4 sm:px-6">
-      <div className="flex flex-col items-center justify-center">
-        <h1 className="text-center text-5xl md:text-7xl text-primary dark:text-primary-dark">
-          <span className="text-red dark:text-yellow">Felicem</span>
-        </h1>
-        <p>the #1 delivery app.</p>
+    <div className="max-w-7xl">
+      <div className="z-50">
+        <GridPattern
+          width={100}
+          height={100}
+          x={-1}
+          y={-1}
+          className={cn(
+            "[mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)] "
+          )}
+        />
       </div>
+      <div className="mx-auto mt-7 px-4 sm:px-6 md:px-12 lg:px-20">
+        <div className="flex flex-col items-center mx-auto">
+          <h1 className="text-5xl md:text-7xl text-primary dark:text-primary-dark">
+            <span className="text-red dark:text-yellow">Felicem</span>
+          </h1>
+          <p>the #1 delivery app.</p>
+        </div>
+      </div>
+
+      {/* we will add bento or grid div style here */}
     </div>
   );
 }
---
+++ components/nav/footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer(){
+
+    return(
+        <div className="mt-80 text-sm text-navy-400 leading-6 pb-8 pt-12 lg:pt-20 text-medium bg-[rgb(25,16,52)] bg-gradient-to-br from-gray-900/50 via-navy-900/50 to-navy-950/50">
+            try
+        </div>
+    )
+}
